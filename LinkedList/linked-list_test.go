c6d5c433187aa7cb4599f021fda5ae5849b6ef69
agoalofalife <agoalofalife@gmail.com>
2017-07-20 22:20:09 +0300

    add test print

--- LinkedList/linked-list_test.go
+++ LinkedList/linked-list_test.go
@@ -106,12 +106,11 @@ func TestGetByIndex(t *testing.T)  {
 	}
 }
 
-func TestExampleF(t *testing.T)  {
-	nodeFirst := &Node{1, nil}
-	nodeSecond := &Node{2, nil}
-	nodeFirst.next = nodeSecond
-
-	Print(nodeFirst)
-	// Output: hello
+func ExamplePrint() {
+		nodeFirst := &Node{1, nil}
+		nodeSecond := &Node{2, nil}
+		nodeFirst.next = nodeSecond
+		Print(nodeFirst)
+	// Output: 1
 }
 // go test -v . | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
