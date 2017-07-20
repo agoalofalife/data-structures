3ec7303bd64128a1bd59ade8240d1be43497f1f1
agoalofalife <agoalofalife@gmail.com>
2017-07-20 16:44:41 +0300

    add method Peel

--- Stack/stack.go
+++ Stack/stack.go
@@ -6,16 +6,17 @@
 package main
 
 import (
-	"os"
 	"log"
+	"os"
 )
 
 type Stack struct {
 	stack []int
 	count int
 }
+
 // push value stack
-func (stack *Stack) Push(value int){
+func (stack *Stack) Push(value int) {
 	stack.stack = append(stack.stack, value)
 	stack.count++
 }
@@ -36,12 +37,20 @@ func (stack *Stack) Count() int {
 	return stack.stack[:1][0]
 }
 
-func main()  {
+// return the value end of the stack
+func (stack *Stack) Peek() (value int) {
+	if stack.count == 0 {
+		panic("Stack is empty")
+	}
+	return stack.stack[len(stack.stack)-1:][0]
+}
+
+func main() {
 	s := Stack{}
 	s.Push(2)
 	s.Push(1)
 	s.Push(3)
 	s.Pop()
-	log.Println(s)
+	log.Println(s.Peek())
 	os.Exit(2)
 }
