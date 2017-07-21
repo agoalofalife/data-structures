7de7b97a35627175a205ed31179181d3c589b1c3
agoalofalife <agoalofalife@gmail.com>
2017-07-21 09:48:26 +0300

    add queue

---
+++ Queue/queue.go
@@ -0,0 +1,40 @@
+//---------------------------------------------------------------------------
+//-[ Queue ]-----------------------------------------------------------------
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+
+package main
+
+import "fmt"
+
+type Queue struct {
+	collection []int
+}
+
+func (queue *Queue) Enqueue(value int) {
+	queue.collection = append(queue.collection, value)
+}
+
+func (queue *Queue) Dequeue() (value int) {
+	value = queue.collection[:1][0]
+	queue.collection = queue.collection[1:]
+	return
+}
+
+func (queue *Queue) Size() int {
+	return len(queue.collection)
+}
+
+func (queue *Queue) IsEmpty() bool {
+	return len(queue.collection) == 0
+}
+
+func main() {
+	// Example FIFO
+	q := Queue{}
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+
+	fmt.Println(q.Dequeue())
+}
