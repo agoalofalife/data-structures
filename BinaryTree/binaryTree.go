3373246dca2687f1eff6931cc8d853476f1d940f
agoalofalife <agoalofalife@gmail.com>
2017-07-24 10:33:43 +0300

    add new structure

---
+++ BinaryTree/binaryTree.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+// Introduction
+// If value Node > then value = 1
+// If value Node < then value = -1
+// else return 0
+func (node *Node) CompareTo(value int) (meter int) {
+	if node.value == 0 {
+		return 0
+	} else if node.value > value {
+		return 1
+	} else if node.value < value {
+		return -1
+	}
+	return meter
+}
+
+type BinaryTree struct {
+	head  *Node
+	count int
+}
+
+func NewBinaryTree() *BinaryTree {
+	tree := &BinaryTree{nil, 0}
+	return tree
+}
+
+func (tree *BinaryTree) Add(value int) {
+	if tree.head == nil {
+		tree.head = &Node{value, nil, nil}
+	} else {
+		tree.addTo(tree.head, value)
+	}
+	tree.count++
+}
+
+func (tree *BinaryTree) addTo(node *Node, value int) {
+	if node.value > value {
+		if node.left == nil {
+			node.left = &Node{value, nil, nil}
+		} else {
+			tree.addTo(node.left, value)
+		}
+	} else {
+		if node.right == nil {
+			node.right = &Node{value, nil, nil}
+		} else {
+			tree.addTo(node.right, value)
+		}
+	}
+}
+
+func (tree *BinaryTree) Remove(value int) {
+
+}
+
+func (tree *BinaryTree) findParent(value int, parent *Node) *Node {
+	current := tree.head
+	for current != nil {
+		meter := current.CompareTo(value)
+		if meter == 1 {
+			parent = current
+			current = current.left
+		} else if meter == -1 {
+			parent = current
+			current = current.right
+		} else {
+			break
+		}
+	}
+	return current
+}
+func main() {
+	tree := NewBinaryTree()
+	tree.Add(2)
+	tree.Add(4)
+	tree.Add(1)
+	s := &Node{}
+	tree.findParent(1, s)
+	fmt.Println(s)
+	os.Exit(0)
+}
