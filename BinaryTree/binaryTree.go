2525c82d6e07cbe7def2133a3e7624a0dc4bec22
agoalofalife <agoalofalife@gmail.com>
2017-07-24 15:13:55 +0300

    fix state

--- BinaryTree/binaryTree.go
+++ BinaryTree/binaryTree.go
@@ -45,6 +45,7 @@ func (tree *BinaryTree) Add(value int) {
 	tree.count++
 }
 
+// helper recursive method for add value in tree
 func (tree *BinaryTree) addTo(node *Node, value int) {
 	if node.value > value {
 		if node.left == nil {
@@ -62,19 +63,58 @@ func (tree *BinaryTree) addTo(node *Node, value int) {
 }
 
 func (tree *BinaryTree) Remove(value int) bool {
-	parent := Node{}
-	current := tree.findWithParent(value, &parent)
+
+	current, parent := tree.findWithParent(value)
+
 	if current == nil {
 		return false
 	}
+	tree.count--
+
+	if current.left == nil && current.right == nil {
+		if parent.left.value == current.value {
+			parent.left = nil
+		} else if parent.right.value == current.value {
+			parent.right = nil
+		}
+	} else if current.right == nil || current.left == nil {
+		current = current.right
+	} else if current.right != nil && current.left != nil {
+
+		leftmost := current.right
+		for leftmost.left != nil {
+			leftmost = leftmost.left
+		}
+
+		if parent.left.value == current.value {
+			parent.left = leftmost
+		} else if parent.right.value == current.value {
+			parent.right = leftmost
+		}
+		parent.left = leftmost
+	}
+
 	return true
 }
 
-func (tree *BinaryTree) findWithParent(value int, parent *Node) *Node {
-	current := tree.head
+func (tree *BinaryTree) Contains(value int) bool {
+	finder, _ := tree.findWithParent(value)
+	return finder != nil
+}
+func (tree *BinaryTree) Count(value int) int {
+	return tree.count
+}
+
+func (tree *BinaryTree) Clear(value int) {
+	tree.head = nil
+	tree.count = nil
+}
+
+// find value position and return value node and his parent node
+func (tree *BinaryTree) findWithParent(value int) (current *Node, parent *Node) {
+	current = tree.head
 	for current != nil {
 		meter := current.CompareTo(value)
-
 		if meter == 1 {
 			parent = current
 			current = current.left
@@ -85,17 +125,18 @@ func (tree *BinaryTree) findWithParent(value int, parent *Node) *Node {
 			break
 		}
 	}
-	//fmt.Println( parent)
-	//os.Exit(0)
-	return current
+	return
 }
+
 func main() {
 	tree := NewBinaryTree()
+	tree.Add(8)
+	tree.Add(5)
+	tree.Add(10)
 	tree.Add(2)
-	tree.Add(4)
-	tree.Add(1)
-	s := &Node{}
-	tree.findWithParent(1, s)
-	fmt.Println(s)
+	tree.Add(7)
+	tree.Add(6)
+	tree.Remove(5)
+	fmt.Println(tree.head.left)
 	os.Exit(0)
 }
