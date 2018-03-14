645d7ed4fd61581dc1a6a9c1dccf1710b7de97fb
agoalofalife <agoalofalife@gmail.com>
2018-03-14 15:24:32 +0300

    add split

--- Rope/rope.go
+++ Rope/rope.go
@@ -15,15 +15,34 @@ func (r *Rope) create(char string) *Rope {
 	return r
 }
 // merge nodes
-func (r *Rope) merge(nodeMerge *Rope) *Rope {
+func merge(nodeFirst *Rope, nodeSecond *Rope) *Rope {
 	newNode := new(Rope)
-	newNode.left = r
-	newNode.right = nodeMerge
+	newNode.left = nodeFirst
+	newNode.right = nodeSecond
 	newNode.char = ""
-	newNode.weight = r.weight + nodeMerge.weight
+	newNode.weight = nodeFirst.weight + nodeSecond.weight
 	return newNode
 }
-
+func split(node *Rope, i int) (*Rope, *Rope) {
+	tree1, tree2 := new(Rope), new(Rope)
+	if node.left != nil {
+		if node.left.weight >= i {
+			tree1, tree2.left = split(node.left, i)
+			tree2.right = node.right
+			tree2.weight = tree2.left.weight + tree2.left.weight
+		} else {
+			tree1.right, tree2 = split(node.right, i - node.left.weight)
+			tree1.left = node.left
+			tree1.weight = tree1.left.weight + tree1.right.weight
+		}
+	} else {
+		tree1.char = node.char[0:i]
+		tree2.char = node.char[i:len(node.char)]
+		tree1.weight = i
+		tree2.weight = len(node.char) - i
+	}
+	return  tree1, tree2
+}
 // get char to index
 func (r *Rope) get(i int) string {
 	// check node is leaf or not
@@ -40,14 +59,15 @@ func (r *Rope) get(i int) string {
 	}
 }
 
+
 func main(){
 	node := new(Rope)
 	node.create("Hello")
+	//
+	//nodeSecond := new(Rope)
+	//nodeSecond.create(" world")
+	//
+	//nodeMerge := merge(node, nodeSecond)
 
-	nodeSecond := new(Rope)
-	nodeSecond.create(" world")
-
-	node.merge(nodeSecond)
-
-	log.Println(node)
+	log.Println(split(node, 2))
 }
