7d695c308921f6704bbaa4553b8cf2c0b05af13b
agoalofalife <agoalofalife@gmail.com>
2018-03-14 12:48:14 +0300

    small change

--- Rope/rope.go
+++ Rope/rope.go
@@ -14,21 +14,40 @@ func (r *Rope) create(char string) *Rope {
 	r.weight = len(char)
 	return r
 }
+// merge nodes
+func (r *Rope) merge(nodeMerge *Rope) *Rope {
+	newNode := new(Rope)
+	newNode.left = r
+	newNode.right = nodeMerge
+	newNode.char = ""
+	newNode.weight = r.weight + nodeMerge.weight
+	return newNode
+}
+
 // get char to index
-func (r *Rope) get(i int, rope *Rope) string {
+func (r *Rope) get(i int) string {
+	// check node is leaf or not
 	if r.left != nil {
+		// node is composite
 		if r.left.weight >= i {
-			return r.get(i, rope.left)
+			return r.get(i)
 		} else {
-			return  r.get(rope.left.weight, rope.right)
+			return r.get(r.left.weight)
 		}
 	} else {
-		return string(rope.char[i])
+		// node is leaf, return char
+		return string(r.char[i])
 	}
 }
 
 func main(){
 	node := new(Rope)
 	node.create("Hello")
+
+	nodeSecond := new(Rope)
+	nodeSecond.create(" world")
+
+	node.merge(nodeSecond)
+
 	log.Println(node)
 }
